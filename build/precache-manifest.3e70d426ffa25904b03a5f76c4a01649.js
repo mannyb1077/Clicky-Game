de9eb00a3d1f03b9dbb41b7c5859c0493b07b3ad
Manny Barboza <mannyb1077@gmail.com>
2019-07-19 20:38:22 -0600

    git pages

---
+++ build/precache-manifest.3e70d426ffa25904b03a5f76c4a01649.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7df488ce8e04976a562",
+    "url": "/Clicky-Game/static/css/main.f6e62d68.chunk.css"
+  },
+  {
+    "revision": "b7df488ce8e04976a562",
+    "url": "/Clicky-Game/static/js/main.be86fc14.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "8b28b04b5b06edd2dc22",
+    "url": "/Clicky-Game/static/js/2.dc948b19.chunk.js"
+  },
+  {
+    "revision": "7c2e5d4ae5e87873a0872084f40d1720",
+    "url": "/Clicky-Game/index.html"
+  }
+];
