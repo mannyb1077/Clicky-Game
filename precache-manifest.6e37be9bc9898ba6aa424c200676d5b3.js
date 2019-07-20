4561e6587b854c8838b4614c08c536f11f000c42
Manny Barboza <mannyb1077@gmail.com>
2019-07-20 13:32:26 -0600

    Updates

---
+++ precache-manifest.6e37be9bc9898ba6aa424c200676d5b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32053993f96b5dc60136",
+    "url": "/Clicky-Game/static/css/main.0eeb5aa6.chunk.css"
+  },
+  {
+    "revision": "32053993f96b5dc60136",
+    "url": "/Clicky-Game/static/js/main.d23d4b74.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "8b28b04b5b06edd2dc22",
+    "url": "/Clicky-Game/static/js/2.dc948b19.chunk.js"
+  },
+  {
+    "revision": "c6f9f71e5c4970968b0aa6d93f2b83df",
+    "url": "/Clicky-Game/index.html"
+  }
+];
